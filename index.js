91fdaf173ed25077a5d11bf350c5eaeb735278b3
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 00:44:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++){
     if (cart[i].itemName === item){
-       cart[i].slice(i, 1);
+       cart[i].splice(i, 1);
     }
   }
 }
